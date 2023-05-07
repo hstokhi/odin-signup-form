9d8915d43dac1b05eb539f149c2f340635c40f97
Hakim Tokhi <hstokhi@yahoo.com>
2023-05-07 15:27:41 -0700

    Build first iteration of sign-up form
    Built sign up form as part of Odin project.
    Used HTML forms and several CSS style elements
    to make the webpage presentable

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-signup-form
+
+Sample sign-up form to practice using intermediate HTML and CSS
---
+++ javascript.js
@@ -0,0 +1,6 @@
+function veryifyPassword () {
+    let password1 = document.getElementById('password')
+    let password2 = document.getElementById('confirm-password')
+
+    
+}
